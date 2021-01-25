5a2b470502454e969698c25605f37e2451c1d393
Hanzhao Yu <yuh24@seas.upenn.edu>
2021-01-25 18:15:37 -0500

    Create hash_collision.py

---
+++ hash_collision.py
@@ -0,0 +1,31 @@
+from hashlib import sha256
+import random
+import hashlib
+import os
+import string
+
+def hash_collision(k):
+    if not isinstance(k,int):
+        print( "hash_collision expects an integer" )
+        return( b'\x00',b'\x00' )
+    if k < 0:
+        print( "Specify a positive number of bits" )
+        return( b'\x00',b'\x00' )
+   
+    #Collision finding code goes here
+    letters = string.ascii_letters + string.digits + string.punctuation
+    x = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
+    y = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
+    while 1:
+        hashedX = sha256(x).hexdigest()
+        hashedY = sha256(y).hexdigest()
+        if hashedY[64-k:64] == hashedX[64-k:64]:
+            break
+        x = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
+        y = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
+    
+    return( x, y )
+
+
+
+
