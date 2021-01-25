f3d3808476e460b5230e1c77f52047e995d4c62e
Hanzhao Yu <yuh24@seas.upenn.edu>
2021-01-25 18:59:52 -0500

    Update hash_preimage.py

--- hash_preimage.py
+++ hash_preimage.py
@@ -5,7 +5,14 @@ def hash_preimage(target_string):
     if not all( [x in '01' for x in target_string ] ):
         print( "Input should be a string of bits" )
         return
-    nonce = b'\x00'
+    letters = string.ascii_letters + string.digits + string.punctuation
+    k = length(target_string)
+    nonce = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
+    while 1:
+        hashed = sha256(nonce).hexdigest()
+        hashed = bin(int(hashed, 16))[2:].zfill(256)
+        if hashed[256-k:256] == target_string:
+            break
+        nonce = ''.join(random.choice(letters) for i in range(10)).encode('utf-8')
 
     return( nonce )
-
