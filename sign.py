b572a17aee90527ba051a93613ab3ce4be421ece
Hanzhao Yu <yuh24@seas.upenn.edu>
2021-02-01 20:32:22 -0500

    Update sign.py

--- sign.py
+++ sign.py
@@ -20,5 +20,5 @@ def sign(m):
     z = sha256(m.encode('utf-8'))
     z = int(z.hexdigest(), 16)
     s = ((modinv(k, n) % n) * ((z+r*pk) % n))%n
-    print(point)
+    print(n * point)
     return( public_key, [r,s] )
