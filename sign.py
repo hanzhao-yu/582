45bf3df64e7cfea6bb42fa98a73c664fa555ff2c
Hanzhao Yu <yuh24@seas.upenn.edu>
2021-02-01 20:30:00 -0500

    Update sign.py

--- sign.py
+++ sign.py
@@ -12,7 +12,7 @@ def sign(m):
     #generate public key
     n = 115792089237316195423570985008687907852837564279074904382605163141518161494337
     (pk, point) = gen_keypair(curve.secp256k1)
-    public_key = point
+    public_key = pk * point
     #generate signature
     k = random.SystemRandom().randint(1,256)
     x = k * point.x
@@ -20,6 +20,4 @@ def sign(m):
     z = sha256(m.encode('utf-8'))
     z = int(z.hexdigest(), 16)
     s = ((modinv(k, n) % n) * ((z+r*pk) % n))%n
-    print(r)
-    print(s)
-    return( public_key, (r,s) )
+    return( public_key, [r,s] )
