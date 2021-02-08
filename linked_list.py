1c455529e1f383b0e5a076f721c9241b53dd77ea
Hanzhao Yu <yuh24@seas.upenn.edu>
2021-02-08 16:27:16 -0500

    Create linked_list.py

---
+++ linked_list.py
@@ -0,0 +1,35 @@
+import hashlib
+class Block:
+    def __init__(self, index, timestamp, content, previous_hash):
+      self.index = index
+      self.timestamp = timestamp
+      self.content = content
+      self.previous_hash = previous_hash
+      self.hash = self.calc_hash()
+   
+    def calc_hash(self):
+      sha = hashlib.sha256()
+      sha.update(str(self.index).encode('utf-8') + 
+                 str(self.timestamp).encode('utf-8') + 
+                 str(self.content).encode('utf-8') + 
+                 str(self.previous_hash).encode('utf-8'))
+      return sha.hexdigest()
+      
+M4BlockChain = []
+
+from datetime import datetime
+def create_genesis_block():
+    return Block(0, datetime.now(), "Genesis Block", "0")
+    
+M4BlockChain.append(create_genesis_block())
+
+
+# write a function `next_block` to generate a block
+def next_block(last_block):
+    i = last_block.index+1
+    return Block(i, datetime.now(), "this is block " + str(i), last_block.hash)
+    
+# append 5 blocks to the blockchain
+def app_five(block_list):
+    for i in range(len(block_list), len(block_list) + 4):
+        block_list.append(next_block(block_list[i-1]))
