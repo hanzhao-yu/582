99a936d99af3b3d60d6a371ee9369cf3b4ff5991
Hanzhao Yu <yuh24@seas.upenn.edu>
2021-04-12 18:03:09 -0400

    Create trace_txes.py

---
+++ trace_txes.py
@@ -0,0 +1,58 @@
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+import json
+from datetime import datetime
+
+rpc_user='quaker_quorum'
+rpc_password='franklin_fought_for_continental_cash'
+rpc_ip='3.134.159.30'
+rpc_port='8332'
+
+rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s"%(rpc_user, rpc_password, rpc_ip, rpc_port))
+
+###################################
+
+class TXO:
+    def __init__(self, tx_hash, n, amount, owner, time ):
+        self.tx_hash = tx_hash 
+        self.n = n
+        self.amount = amount
+        self.owner = owner
+        self.time = time
+        self.inputs = []
+
+    def __str__(self, level=0):
+        ret = "\t"*level+repr(self.tx_hash)+"\n"
+        for tx in self.inputs:
+            ret += tx.__str__(level+1)
+        return ret
+
+    def to_json(self):
+        fields = ['tx_hash','n','amount','owner']
+        json_dict = { field: self.__dict__[field] for field in fields }
+        json_dict.update( {'time': datetime.timestamp(self.time) } )
+        if len(self.inputs) > 0:
+            for txo in self.inputs:
+                json_dict.update( {'inputs': json.loads(txo.to_json()) } )
+        return json.dumps(json_dict, sort_keys=True, indent=4)
+
+    @classmethod
+    def from_tx_hash(cls,tx_hash,n=0):
+        tx = rpc_connection.getrawtransaction(tx_hash,True)
+        self.tx_hash = tx_hash
+        self.n = n
+        vout = tx.get("result").get("vout")
+        self.amount = vout[n].get("value")
+        self.owner = vout[n].get("address")
+        self.time = datetime.fromtimestamp(tx.get("result").get("blocktime"))
+
+    def get_inputs(self,d=1):
+        tx = rpc_connection.getrawtransaction(self.tx_hash,True)
+        vin = vout = tx.get("result").get("vin")
+        l = len(vin)
+        idx = 0
+        while idx < l and idx < d:
+            txo = TXO()
+            txo.from_tx_hash(vin[idx].get("txid"), vin[idx].get("vout"))
+            self.inputs.append(txo)
+
+
